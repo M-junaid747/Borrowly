01c317bc4e060dad305c3dc799ef2ba016c92b8c
M-junaid747 <muhammadjunaid0802@gmail.com>
2026-07-28 06:13:21 +0500

    Fix Render build failure: switch to non-manifest WhiteNoise storage

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -91,6 +91,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
